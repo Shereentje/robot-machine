1c47ba9a9df2d1c5880c92e0d06e82b67430a77d
Shereentje <155670114+Shereentje@users.noreply.github.com>
2026-04-07 15:07:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Robot Machine</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
+function play(playerChoice) {
+    const robot = robotChoice();
+    let resultMessage = "";
 
-  <body>
-    <header>
-      <h1>Rock, Paper, Scissors</h1>
+    if (playerChoice === robot) {
+        resultMessage = "Gelijkspel";
+    } else if (
+        (playerChoice === "rock" && robot === "scissors") ||
+        (playerChoice === "paper" && robot === "rock") ||
+        (playerChoice === "scissors" && robot === "paper")
+    ) {
+        resultMessage = Goed gedaan ${playerName}! Jij wint 🎉;
+        playerScore++;
 
-      <div id="startScreen">
-        <div>🤖</div>
-        <h2>Hi speler!</h2>
-        <p>Voer je naam in:</p>
+        // Confetti bij winst
+        launchConfetti();
 
-        <input type="text" id="nameInput" placeholder="Jouw naam...">
-        <button onclick="startGame()">Start</button>
-      </div>
-    </header>
+    } else {
+        resultMessage = "De robot wint 🤖";
+        robotScore++;
+    }
 
-    <div id="gameScreen" hidden>
-      <h2 id="welcomeText"></h2>
+    resultText.innerText = ${resultMessage} (${playerChoice} vs ${robot});
 
-      <div>
-        <img src="img/rock.png" onclick="play('rock')" />
-        <img src="img/paper.png" onclick="play('paper')" />
-        <img src="img/scissors.png" onclick="play('scissors')" />
-      </div>
-
-      <div id="result">Maak je keuze!</div>
-      <div id="score">0 - 0</div>
-    </div>
-
-    <!--bron: https://www.youtube.com/watch?v=k5BJoJ9bRBE confetti tutorial-->
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js"></script>
-    <script src="script.js"></script>
-  </body>
-</html>
+    updateScore();
+}
